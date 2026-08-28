87e93580a7267fcb5f1855b6ae11a5b7e5ee6f37
Duc_Tien <buiductien90@gmail.com>
2026-08-28 14:09:34 +0200

    Xoa toan bo

--- math_utils.cpp
+++
@@ -1,8 +0,0 @@
-
-// File: math_utils.cpp
-#include "math_utils.h"
-
-// Thân hàm chi tiết
-double tinhHieu(double a, double b) {
-    return a - b;
-}
--- math_utils.h
+++
@@ -1,5 +0,0 @@
-// File: math_utils.h
-#pragma once  // Header Guard: bảo vệ chống include trùng
-
-// Nguyên mẫu hàm: Chỉ báo trước kiểu trả về, tên hàm, tham số
-double tinhHieu(double a, double b);
